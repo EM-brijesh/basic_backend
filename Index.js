a927fc1eb40ce11fc29468d065dc574d5f14c565
Brijesh Kori <Brijesh.Kori@everymedia.com>
2024-10-24 14:20:11 +0530

    setup

---
+++ Index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+console.log(process.env.MONGO_URL)
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { userRouter } = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/v1/user", userRouter);
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
